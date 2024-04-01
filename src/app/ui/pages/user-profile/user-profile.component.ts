e9a2f6986001ed4d5426dae651dd2def2d38a85c
anyistefaniabecerra <anyistefaniabecerra@gmail.com>
2024-04-01 12:08:39 -0500

    profile of customer is created

--- src/app/ui/pages/user-profile/user-profile.component.ts
+++ src/app/ui/pages/user-profile/user-profile.component.ts
@@ -1,18 +1,16 @@
 import { Component } from '@angular/core';
 import { Profile } from '../../interfaces/profile';
 import { UserPreferencesService } from '../../../shared/services/user-preferences.service';
-import { NgbRating } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [NgbRating],
+  imports: [],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
 export class UserProfileComponent {
   public dataProfile: Profile = {} as Profile;
-  public rating = 8;
   constructor(private profileBehaviorService: UserPreferencesService) {}
 
   ngOnInit() {
