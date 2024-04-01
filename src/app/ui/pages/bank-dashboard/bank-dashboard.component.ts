d5ccb91ad4e0d57088f0dc2325f3e0283ced6f69
anyistefaniabecerra <anyistefaniabecerra@gmail.com>
2024-04-01 13:32:36 -0500

    refactor dashboard

--- src/app/ui/pages/bank-dashboard/bank-dashboard.component.ts
+++ src/app/ui/pages/bank-dashboard/bank-dashboard.component.ts
@@ -1,20 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Profile } from '../../interfaces/profile';
 import { UserPreferencesService } from '../../../shared/services/user-preferences.service';
 import { TransactionsUserDataService } from '../../services/transactions-user-data.service';
 import { Transaction } from '../../interfaces/transaction';
+import { catchError, take } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bank-dashboard',
-  standalone: true,
-  imports: [CommonModule],
+  imports:[CommonModule],
+  standalone:true,
   templateUrl: './bank-dashboard.component.html',
-  styleUrl: './bank-dashboard.component.css'
+  styleUrls: ['./bank-dashboard.component.css']
 })
-export class BankDashboardComponent {
+export class BankDashboardComponent implements OnInit {
   public dataProfile: Profile = {} as Profile;
   public transactionData: Transaction[] = [];
+
   constructor(
     private profileBehaviorService: UserPreferencesService,
     private transactionsService: TransactionsUserDataService
@@ -32,28 +35,28 @@ export class BankDashboardComponent {
   }
 
   getTransactions(): void {
-    this.transactionsService.getTransactions().subscribe({
-      next: (data: Transaction[]) => {
-        this.transactionData = data.slice(0, 10);
-      },
-      error: (error) => {
+    this.transactionsService.getTransactions().pipe(
+      take(1), 
+      catchError(error => {
         console.error('There was an error!', error);
-      },
+        return throwError(() => new Error(error));
+      })
+    ).subscribe((data: Transaction[]) => {
+      this.transactionData = data.slice(0, 10);
     });
   }
 
   get transactionAmount(): number {
     return this.transactionData.reduce(
-      (acumulador, transaccion) => acumulador + transaccion.amount,
+      (accumulator, transaction) => accumulator + transaction.amount,
       0
     );
   }
 
   get transactionBalance(): number {
     return this.transactionData.reduce(
-      (acumulador, transaccion) => acumulador + transaccion.balance,
+      (accumulator, transaction) => accumulator + transaction.balance,
       0
     );
   }
-
 }
