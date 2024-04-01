92740adbb02b54cfe572133a6be2b3c845115b5d
anyistefaniabecerra <anyistefaniabecerra@gmail.com>
2024-04-01 02:38:47 -0500

    adjusting some details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,9 +13,12 @@ export const routes: Routes = [
   {
     path: '',
     component: GlobalComponent,
-
     children: [
-
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full',
+      },
       {
         path: 'home',
         component: HomeComponent,
